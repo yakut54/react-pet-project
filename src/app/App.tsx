38df93834579a69c9fb8854ae67e7a7f12902a13
yakut54 <mongodb1@yandex.ru>
2023-04-16 21:37:29 +0700

    Navbar UI AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,14 @@
-import {Link} from 'react-router-dom'
 import {useTheme} from 'app/providers/ThemeProvider'
 import {classNames} from 'shared/lib/classNames/classNames'
 import {AppRouter} from 'app/providers/router'
+import {Navbar} from 'widgets/Navbar'
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О Сайте</Link>
+      <Navbar />
       <button onClick={toggleTheme}>click</button>
 
       <AppRouter />
