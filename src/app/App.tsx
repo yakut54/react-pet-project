fb19b93165c9b90a390b95832a126c8a5460252f
yakut54 <mongodb1@yandex.ru>
2023-04-18 07:43:07 +0700

    Sidebar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import {useTheme} from 'app/providers/ThemeProvider'
 import {classNames} from 'shared/lib/classNames/classNames'
 import {AppRouter} from 'app/providers/router'
 import {Navbar} from 'widgets/Navbar'
+import {Sidebar} from 'widgets/Sidebar'
 
 const App = () => {
   const {theme} = useTheme()
@@ -9,7 +10,12 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className={'content-page'}>
+        <Sidebar />
+        <div className={'page-wrapper'}>
+          <AppRouter />
+        </div>
+      </div>
     </div>
   )
 }
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -11,7 +11,6 @@ interface NavbarProps {
 export const Navbar = ({className}: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.mainLink}>Главная</AppLink>
         <AppLink theme={AppLinkTheme.SECONDARY} to={'/about'}>О Сайте</AppLink>
