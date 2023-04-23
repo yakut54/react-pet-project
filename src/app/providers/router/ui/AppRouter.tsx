5d12ff9f8c85d800b8e9afa8345398dbf11d42b1
yakut54 <mongodb1@yandex.ru>
2023-04-24 06:50:09 +0700

    NotFoundPage

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,13 +1,12 @@
-import React, { Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
-import { routerConfig } from 'shared/config/routeConfig/routeConfig'
-import { useTranslation } from 'react-i18next'
+import React, {Suspense} from 'react'
+import {Route, Routes} from 'react-router-dom'
+import {routerConfig} from 'shared/config/routeConfig/routeConfig'
+import {PageLoader} from 'widgets/PageLoader/PageLoader'
 
 export const AppRouter = () => {
-  const { t } = useTranslation()
 
   return (
-    <Suspense fallback={<div>{t('Загрузка...')}</div>}>
+    <Suspense fallback={<PageLoader />}>
       <Routes>
         {Object.values(routerConfig).map(({ element, path }) => (
           <Route path={path} element={element} key={path} />
